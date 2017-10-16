7e145e44ef55befc23757462d5fb11230e9b2f38
masariuman <masariuman@gmail.com>
2017-10-17 06:17:22 +0700

    master

---
+++ interaktif.rb
@@ -0,0 +1,5 @@
+puts "Halo, nama kamu siapa?"
+nama = gets
+
+puts "Hai, nama saya "+nama
+puts "Hai, nama saya #{nama} "
---
+++ variable.rb
@@ -0,0 +1,10 @@
+mahasiswa = 'Arif Setiawan'
+nilai = 90
+
+puts mahasiswa
+puts nilai
+
+angka_1 = 100
+angka_2 = 50
+hasil = angka_1 / angka_2
+puts hasil
