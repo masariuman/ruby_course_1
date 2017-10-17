9d8d500e6b0d3717327e3057b829064464be00a4
masariuman <masariuman@gmail.com>
2017-10-17 14:14:25 +0700

    second

---
+++ scope.rb
@@ -0,0 +1,30 @@
+#local variabel
+#10.times do
+#  x = 5
+#  puts x
+#end
+
+
+#global variabel
+#5.times do
+#  $x = 90
+#end
+#puts $x
+
+
+#instance variabel
+#@news = News.new
+#news = News.new
+
+
+#class variabel
+#@@nama_variabel
+
+
+#constant
+#untuk variabel tetap.. contoh nilai PHI atau kita menetapkan variabel harga
+# HARGA = 10000
+# konstant bisa di ubah
+# HARGA = 10000
+# HARGA = 10001
+# puts HARGA
