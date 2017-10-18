5a5bba53f0408ba30ef4ec0b74a34b7dbd7c80b4
masariuman <masariuman@gmail.com>
2017-10-18 08:20:05 +0700

    interpolation

---
+++ interpolation.rb
@@ -0,0 +1,14 @@
+nama = 'arif'
+bahasa = 'ruby'
+
+puts "Nama saya #{nama}, bahasa saya #{bahasa}"
+
+puts "Hasil = #{10 * 2}"
+
+angka = 10
+
+angka2 = 2
+
+hasil = angka * angka2
+
+puts "hasil 2 = #{hasil}"
