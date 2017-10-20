27cfa2e7ca52a22671d0fa0377fbe6378d6fedf9
masariuman <masariuman@gmail.com>
2017-10-20 10:40:43 +0700

    case.rb

---
+++ case.rb
@@ -0,0 +1,18 @@
+jenis_kelamin = 'p'
+
+if jenis_kelamin == 'p'
+  puts 'Perempuan'
+elsif jenis_kelamin == 'l'
+  puts 'Laki-laki'
+else
+  puts 'Ga jelas'
+end
+
+case jenis_kelamin
+when 'p'
+  puts 'Perempuan'
+when 'l'
+  puts 'Laki-laki'
+else
+  puts 'Ga jelas'
+end
