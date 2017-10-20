734a9d374d5f101188cdfb8e772676319150a231
masariuman <masariuman@gmail.com>
2017-10-20 13:44:50 +0700

    opsional method

---
+++ return_value.rb
@@ -0,0 +1,9 @@
+def penjumlahan(nilai_a, nilai_b)
+  return nilai_a - nilai_b if nilai_a == 5
+  nilai_a + nilai_b
+end
+
+hasil_satu = penjumahan(6, 5)
+hasil_dua = penjumlahan(10, 10)
+hasiil_tiga = hasil_satu * hasil_dua
+puts hasil_tiga
