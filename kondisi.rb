ca1d59a4b1e674668648c704056861863bfca502
masariuman <masariuman@gmail.com>
2017-10-20 10:17:15 +0700

    kondisi

---
+++ kondisi.rb
@@ -0,0 +1,14 @@
+nilai = 70
+password = 'password'
+
+if nilai >= 80
+  puts 'Selamat kamu lulus ujian'
+end
+
+if password == 'password'
+  puts 'kamu berhasil login'
+else
+  puts 'kamu gagal login'
+end
+
+puts 'password benar' if password == 'password'
