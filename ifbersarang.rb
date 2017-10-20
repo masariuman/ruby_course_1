78723e3b69e7c3bb75c303eaac8282fa2b9b7105
masariuman <masariuman@gmail.com>
2017-10-20 10:32:55 +0700

    ifbersarang.rb

---
+++ ifbersarang.rb
@@ -0,0 +1,18 @@
+jenis_kelamin ='l'
+umur = 27
+
+if jenis_kelamin == 'l'
+  if umur <= 27
+    puts 'kamu laki-laki muda'
+  else
+    puts 'kamu laki-laki tua'
+  end
+elsif jenis_kelamin == 'p'
+  if umur <= 25
+    puts 'kamu perempuan muda'
+  else
+    puts 'kamu perempuan muda juga sih'
+  end
+else
+  puts 'kamu jenis kelaminnya apa ya ?'
+end
