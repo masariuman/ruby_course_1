5f885f91e216ad69a295dd62307c93282990df1f
masariuman <masariuman@gmail.com>
2017-10-20 10:26:44 +0700

    elsif

---
+++ elseif.rb
@@ -0,0 +1,20 @@
+#A >= 85
+#B >= 70
+#c >= 60
+#D >= 50
+#E SISANYA
+
+
+nilai = 75
+
+if nilai >= 85
+  puts 'A'
+elsif nilai >= 70
+  puts 'B'
+elsif nilai >= 60
+  puts 'C'
+elsif nilai >= 50
+  puts 'D'
+else
+  puts 'E'
+end
